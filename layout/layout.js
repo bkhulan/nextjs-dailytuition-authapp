32405206b96486a4ef72745c2e68fe78b15fff44
khulan <bayasgalankhulan79@gmail.com>
2023-05-12 10:41:14 +0800

    Delete src/app/pages.js, add _app.js, index.js, login.js, register.js and layout.js.

---
+++ layout/layout.js
@@ -0,0 +1,9 @@
+function Layout({ children }) {
+  return (
+    <div  className="flex h-screen bg-blue-400">
+      {children}
+    </div>
+  );
+}
+
+export default Layout;
---
+++ src/pages/login.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Layout from "../../layout/layout";
+
+function Login() {
+  return (
+    <Layout>
+      <Head>
+        <title>Login Page</title>
+      </Head>
+      <main>
+        <h1>Login!</h1>
+      </main>
+    </Layout>
+  );
+}
+
+export default Login;
