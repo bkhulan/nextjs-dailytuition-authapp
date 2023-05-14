3796e4cdb38c70d64dd14445312a1bd1eda49e62
khulan <bayasgalankhulan79@gmail.com>
2023-05-14 10:27:10 +0800

    Add user and guest function in index.js.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,63 @@
+import { useState } from "react";
 import Head from "next/head";
+import Link from "next/link";
+
+import styles from "../../styles/Home.module.css";
 
 function Home() {
+  const [session, setSession] = useState(false);
+
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>Home Page</title>
       </Head>
-      <main>
-        <h1>Hello world!</h1>
-      </main>
+      {session ? User() : Guest()}
     </div>
+  );
+}
+
+function Guest() {
+  return (
+    <main className="container mx-auto text-center py-20">
+      <h3 className="text-4xl font-bold">Guest Homepage</h3>
+      <div className="flex justify-center">
+        <Link
+          href={"/login"}
+          className="mt-5 px-10 py-1 rounded-sm bg-indigo-500 text-gray-200"
+        >
+          Sign In
+        </Link>
+      </div>
+    </main>
+  );
+}
+
+function User() {
+  return (
+    <main className="container mx-auto text-center py-20">
+      <h3 className="text-4xl font-bold">Authorize User Homepage</h3>
+
+      <div className="details">
+        <h5>Unknown</h5>
+        <h5>Unknown</h5>
+      </div>
+
+      <div className="flex justify-center">
+        <button className="mt-5 px-10 py-1 rounded-sm bg-indigo-500 bg-gray-50">
+          Sign Out
+        </button>
+      </div>
+
+      <div className="flex justify-center">
+        <Link
+          href={"/profile"}
+          className="mt-5 px-10 py-1 rounded-sm bg-indigo-500 text-gray-200"
+        >
+          Profile Page
+        </Link>
+      </div>
+    </main>
   );
 }
 
